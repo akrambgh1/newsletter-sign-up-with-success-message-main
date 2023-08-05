31c1e2b80066cfb31401b2d45891ca34887e18c9
akram <akrambgh316@gmail.com>
2023-08-04 20:28:54 -0700

    ghgh

---
+++ java.js
@@ -0,0 +1,8 @@
+function hi() {
+    document.querySelector(".page-one").style.display="none";
+    document.querySelector(".page-two").style.display="flex";
+    const x = document.querySelector("input").value
+    document.querySelector(".page-two span").innerHTML= x
+    };
+
+
